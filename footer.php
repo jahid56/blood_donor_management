ad1e300e17561ed304d59116d470b5899bc596ed
Jahid Mahmud <influencecse78@gmail.com>
2016-10-24 18:20:41 +0600

    Perfectly Works All

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
 <footer class="page-footer blue center-on-small-only">
             <div class="footer-copyright text-center rgba-black-light">
                 <div class="container-fluid">
-                    © 2016 Copyright: <a href="http://hamitire.byethost10.com/"> OHSolutions.com </a>
+                    © 2016 Copyright: <a href=""> Blood Donor System </a>
                 </div>
             </div>
         </footer>
